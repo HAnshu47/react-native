1c35a17bbd6754d29924d3b59e7e1e06d7d611fa
Huangrui47 <1477315575@qq.com>
2025-08-06 10:49:38 +0800

    修改入口文件

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function index() {
+
+ return (
+ 
+<View >
+ 
+ <Text>123</Text>
+ <Text>123</Text>
+ <Text>123</Text>
+ 
+</View>
+);
+}
+const styles = StyleSheet.create({ });
