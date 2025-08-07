1429806d5e6de93221b7526699387de4923d65c1
Huangrui47 <1477315575@qq.com>
2025-08-07 11:15:18 +0800

    修改tabs-icon

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text, useColorScheme } from 'react-native'
 import React from 'react'
 import { Stack, Tabs } from 'expo-router'
 import { Colors } from '../../assets/color/color'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function _layout() {
   //根
@@ -9,9 +10,24 @@ export default function _layout() {
   return (
     <Tabs screenOptions={{
       headerShown: false,
-        tabBarActiveTintColor: theme.iconColorFocused,
-        tabBarInactiveTintColor: theme.iconColor,
-        tabBarStyle: { backgroundColor: theme.background },
-    }} />
+      tabBarActiveTintColor: theme.iconColorFocused,
+      tabBarInactiveTintColor: theme.iconColor,
+      tabBarStyle: { backgroundColor: theme.background },
+    }} >
+      <Tabs.Screen name="text" options={{
+        title: '文本',
+        tabBarIcon: ({ color, size, focused }) => (
+          <Ionicons name={focused ? 'person' : 'person-outline'} size={size} color={color} />
+        )
+      }}>
+      </Tabs.Screen>
+      <Tabs.Screen name="time" options={{
+        title: '时间',
+        tabBarIcon: ({ color, size, focused }) => (
+          <Ionicons name={focused ? 'time' : 'time-outline'} size={size} color={color} />
+        )
+      }}>
+      </Tabs.Screen>
+    </Tabs>
   )
 }
